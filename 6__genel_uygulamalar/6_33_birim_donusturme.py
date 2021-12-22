07fc05a01897b0bfd63e32013319fa0c438dc659
Berkant Beşmart <berkant.5m@hotmail.com>
2021-12-22 14:26:54 +0300

    Add files via upload

---
+++ 6__genel_uygulamalar/6_33_birim_donusturme.py
@@ -0,0 +1,15 @@
+# Birim dönüştürme
+a=eval(input("Uzunluğu giriniz (m): "))
+print("n\Dönüştürme\n1- nm\n2- cm\n3- dm\n4- km\n")
+sec=eval (input("Seçiminiz: "));
+if (sec==1):
+    b=1000*a
+elif (sec==2):
+    b=100*a
+elif (sec==3):
+    b=10*a
+elif (sec==4):
+    b=a/1000
+else:
+    b=0
+print("\nSonuç: %0.5f"%b)
