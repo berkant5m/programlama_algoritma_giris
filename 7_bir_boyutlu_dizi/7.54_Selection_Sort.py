11bf915c8049c926a9dcafcaf6a4005d04cdaa95
Berkant Beşmart <berkant.5m@hotmail.com>
2022-01-12 15:43:33 +0300

    Add files via upload

---
+++ 7_bir_boyutlu_dizi/7.54_Selection_Sort.py
@@ -0,0 +1,15 @@
+# Dizi Elemanlarını Sıralama - Selection sort
+A=[]
+N=int(input("Dizinin eleman sayısnı giriniz: "))
+for i in range(N):
+  print("A(%d)= "%(i+1),end='')
+  A.append(int(input()))
+for i in range(N-1):
+  for j in range(i+1,N):
+    if (A[j]<A[i]):
+      g=A[i]
+      A[i]=A[j]
+      A[j]=g
+print("\nSıralı dizi\n===========")
+for i in range(N):
+  print("A(%d)=%d"%(i+1,A[i]))
